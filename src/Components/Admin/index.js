7a8681c9a46927292dd97e59a044a0595fc02177
EMTIE23 <matttie23@icloud.com>
2021-12-22 03:42:00 +1100

    Revert "Revert "placeholder admin page""
    This reverts commit cda346d332ca20df32153ea4891935e3eb4bc91e.

---
+++ src/Components/Admin/index.js
@@ -0,0 +1,16 @@
+import Sidebar from "../Sidebar";
+import React from "react";
+import { withRouter } from "react-router-dom"
+
+const Admin = () => {
+
+
+    return(
+    <div>
+
+
+    </div>
+    )
+}
+
+export default Admin
