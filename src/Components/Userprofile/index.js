cbc2c22b79df8af251b57e7a62c828bb89861b34
EMTIE23 <72491324+EMTIE23@users.noreply.github.com>
2022-01-26 04:03:23 +1100

    User_profile complete
    structure succesful, page made to wireframe requirement, ready for pull request

--- src/Components/Userprofile/index.js
+++ src/Components/Userprofile/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./userprofile.module.scss"
 // import style from "./security.module.scss";
+ let userIcon = "https://freesvg.org/img/abstract-user-flat-4.png"
 
 const Userprofile = ({}) => {
 
@@ -11,16 +12,20 @@ const Userprofile = ({}) => {
 
             <section className={style.UserInfo}>
             <p><label className={style.usrname} >Username:&nbsp;Sample</label></p>              
-            <p><label>Roles:&nbsp;Sample</label></p>
+            <p><label className={style.roles}>Roles:&nbsp;Sample</label></p>
             </section>
             
-            <section>
+             <section className={style.profilechange}>
+            <p><img className={style.userIcon} src={userIcon} alt="user pic"/></p>
+            <a href="url"className={style.hyperlink}>change</a>
+            </section>
+            
+            <section >
                 <label>Name:</label>
-                <input className={style.inputactual}  type="text" ></input> 
+                <input className={style.inputactual}  type="text" ></input>                 
             </section>
-            
-
-             <section>
+        
+            <section >
                 <label>Email:</label>
                 <input className={style.inputactual} type="text" ></input> 
             </section>
@@ -30,12 +35,14 @@ const Userprofile = ({}) => {
                 <input className={style.inputpass} type="text" ></input> 
                 <button className= {style.change}> Change Password</button> 
             </section>
+
             
+            
+           
 
-
-            <div>
+           
                 <button className={style.save} type="button">Save</button>
-            </div>
+            
 
         
     </form>
