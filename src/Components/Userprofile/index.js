6cf794fc82d45c73e1295ebe504c8e205fcabdd1
EMTIE23 <72491324+EMTIE23@users.noreply.github.com>
2022-02-21 11:59:21 +1100

    Format changes made
    branch ready for pull

--- src/Components/Userprofile/index.js
+++ src/Components/Userprofile/index.js
@@ -1,48 +1,42 @@
 import React from "react";
-import style from "./userprofile.module.scss"
+import style from "./userprofile.module.scss";
 // import style from "./security.module.scss";
 
 const Userprofile = ({}) => {
-
-    return (
-
+  return (
     <form className={style.form}>
-        
+      <section className={style.UserInfo}>
+        <p>
+          <label>Username:&nbsp;Sample</label>
+        </p>
+        <p>
+          <label>Roles:&nbsp;Sample</label>
+        </p>
+      </section>
 
-            <section className={style.UserInfo}>
-            <p><label>Username:&nbsp;Sample</label></p>              
-            <p><label>Roles:&nbsp;Sample</label></p>
-            </section>
-            
-            <section>
-                <label>Name:</label>
-                <input className={style.inputactual}  type="text" ></input> 
-            </section>
-            
+      <section>
+        <label>Name:</label>
+        <input className={style.inputactual} type="text"></input>
+      </section>
 
-             <section>
-                <label>Email:</label>
-                <input className={style.inputactual} type="text" ></input> 
-            </section>
+      <section>
+        <label>Email:</label>
+        <input className={style.inputactual} type="text"></input>
+      </section>
 
-            <section>
-                <label>Password:</label>
-                <input className={style.inputpass} type="text" ></input> 
-                <button className= {style.change}> Change Password</button> 
-            </section>
-            
+      <section>
+        <label>Password:</label>
+        <input className={style.inputpass} type="text"></input>
+        <button className={style.change}> Change Password</button>
+      </section>
 
-
-            <div>
-                <button className={style.save} type="button">Save</button>
-            </div>
-
-        
+      <div>
+        <button className={style.save} type="button">
+          Save
+        </button>
+      </div>
     </form>
-
-    )
-
-}
+  );
+};
 
 export default Userprofile;
-
