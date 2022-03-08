30c8b1e7ad6717fe80f84b69eebeaf1835c1c345
Julian Nunez <julian.nunez@towcha.com.au>
2022-03-09 09:30:52 +1100

    Makes changes in Councils page in order to show the data. Also cleans the code and deletes unnecessary files.

--- src/Components/Locations/index.js
+++ src/Components/Locations/index.js
@@ -1,43 +1,49 @@
 import React, { useState, useEffect } from 'react'
-
+import style from "./location.module.scss"
 import Table from "../Common/Table/Table"
 import { onDelete, onEdit } from "../Common/Table/APIUtil"
 
-import style from "./location.module.scss"
 
-
-const deleteURL = "/api/council"
+const baseURL = "/api/council"
+const fetchURL = baseURL// + "/?active=0"
 const editURl = "/locations/council/edit"
 
+const groupBy = keys => array =>
+  array.reduce((objectsByKeyValue, obj) => {
+    const value = keys.map(key => obj[key]).join('-');
+    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+    return objectsByKeyValue;
+  }, {});
+
 const Council = ({ history, location }) => {
-
-
     const [state, setState] = useState("All")
     const [councils, setCouncils] = useState([])
+    const cols = ["ShortName", "Name", "State", "Communities", "Active"]
 
-    const regions = ["All", "NT", "QLD", "SA", "WA"]
+    // Set regions array dinamically based on the councils list
+    let regions = ["All"]
+    let groupByState = councils.reduce(function (r, a) {
+                            r[a.State] = r[a.State] || [];
+                            r[a.State].push(a);
+                            return r;
+                        }, Object.create(null));
+    regions = regions.concat(Object.keys(groupByState).sort())
 
-    const cols = ["ShortName", "Name", "State", "Communities"]
-
-
-    const fetchCouncil = async () => {
+    const fetchCouncils = async () => {
         try {
-            let res = await fetch("/api/council")
+            let res = await fetch(fetchURL)
             res = await res.json();
-            if (res.error) {
-                console.log(res.error)
-            } else { setCouncils(res.data) }
-
+            if (res.error)
+                console.error(res.error)
+            else
+                setCouncils(res.data)
         } catch (error) {
-            console.log(error)
+            console.error(error)
         }
-
     }
 
-
-
     useEffect(() => {
-        fetchCouncil()
+        fetchCouncils()
     }, [])
 
     const onClick = (e) => {
@@ -49,13 +55,14 @@ const Council = ({ history, location }) => {
         return false
     }
 
-
-    const deleteItem = onDelete(deleteURL, fetchCouncil)
+    const deleteItem = onDelete(baseURL, fetchCouncils)
     const editItem = onEdit(history, editURl)
 
     const getDisplayData = () => {
-        let res = state === "All" ? councils : councils.filter(item => item.State === state)
-        return res.filter(item => item.Active === true)
+        let activeCouncils = councils.filter(council => council.Active)
+        if (state === "All")
+            return activeCouncils
+        return activeCouncils.filter(council => council.State === state)
     }
 
     return (
@@ -64,11 +71,11 @@ const Council = ({ history, location }) => {
                 <button className="bt-add" onClick={onClick} > Add </button>
                 <div className={style.regions}>
                     {regions.map((r, idx) => (
-                        <span
-                            key={r}
-                            className={state === r ? style.current : null}
-                            onClick={(e) => handleRegionChange(e, r)}
-                        > {r} </span>
+                        <span key={r}
+                              className={state === r ? style.current : null}
+                              onClick={(e) => handleRegionChange(e, r)}>
+                            {r}
+                        </span>
                     ))}
                 </div>
                 <span></span>
@@ -78,16 +85,9 @@ const Council = ({ history, location }) => {
                 cols={cols}
                 onEdit={editItem}
                 onDelete={deleteItem}
-                pk={"CouncilId"}
-            />
+                pk={"Id"}/>
         </div>
     )
 }
 
-
 export default Council
-
-
-// Mock Data
-
-
